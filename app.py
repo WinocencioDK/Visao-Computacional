d91eb287c84619b97c04e7771747652de59de255
Willian Inocencio <willian.i.alves@hotmail.com>
2019-08-31 15:27:05 -0300

    alteracao front

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/envio')
 def index():
     return render_template('envioImagem.html')
+
+@app.route('/resposta')
+def resposta():
+    return render_template('resposta.html')
+
