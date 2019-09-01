08e7c62c531cc4c3f1106bf715b884f6dfa7f4a5
Willian Inocencio <willian.i.alves@hotmail.com>
2019-09-01 20:28:59 -0300

    Adicionado Google Vision API

--- app.py
+++ app.py
@@ -1,27 +1,55 @@
-import os
+import os , io
 import urllib.request
+from face import Face
 from flask import Flask, render_template , flash, request
+from google.cloud import vision
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-UPLOAD_FOLDER = '/home/willian'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="client_secrets.json"
+UPLOAD_FOLDER = '/home/willian/'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/envio')
-def index():
+def index():   
     return render_template('envioImagem.html')
 
 @app.route('/renderImage' ,  methods=['POST'])
 def renderImage():
-    print("HELLO")
+
+    client = vision.ImageAnnotatorClient()
 
     if request.method == 'POST':
         file = request.files['file']
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
 
-    return render_template('resposta.html')
+        with io.open(UPLOAD_FOLDER + file.filename, 'rb') as image_file:
+            content = image_file.read()
+
+        image = vision.types.Image(content=content)
+
+        response = client.face_detection(image=image)
+        faces = response.face_annotations
+
+        # Names of likelihood from google.cloud.vision.enums
+        likelihood_name = ('DESCONHECIDO', 'MUITO_IMPROVAVEL', 'IMPROVAVEL', 'POSSIVEL',
+                        'PROVAVEL', 'MUITO_PROVAVEL')
+        
+        
+        rostos = []
+        i = 1
+        for face in faces:
+
+            vertices = (['({},{})'.format(vertex.x, vertex.y)
+            for vertex in face.bounding_poly.vertices])
+
+            rosto = Face(i, likelihood_name[face.joy_likelihood], likelihood_name[face.sorrow_likelihood],likelihood_name[face.surprise_likelihood],likelihood_name[face.anger_likelihood],vertices)
+            rostos.append(rosto)
+            i = i + 1
+
+    return render_template('resposta.html', rostos=rostos)
 
 if __name__ == '__main__':
     app.run(host='192.168.0.8', port=3001)
---
+++ face.py
@@ -0,0 +1,8 @@
+class Face:
+    def __init__(self, id, feliz, triste,surpreso,raiva,vertices):
+        self.id = id
+        self.feliz = feliz
+        self.triste = triste
+        self.surpreso = surpreso
+        self.raiva = raiva
+        self.vertices = vertices
