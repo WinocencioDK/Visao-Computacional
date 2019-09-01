c2fc1a10ddedf24ba1e9f163a9a560fde108a51d
Willian Inocencio <willian.i.alves@hotmail.com>
2019-08-31 21:06:34 -0300

    Envio da imagem para servidor

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+import os
+import urllib.request
+from flask import Flask, render_template , flash, request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
+
+UPLOAD_FOLDER = '/home/willian'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 @app.route('/envio')
 def index():
     return render_template('envioImagem.html')
 
-@app.route('/resposta')
-def resposta():
+@app.route('/renderImage' ,  methods=['POST'])
+def renderImage():
+    print("HELLO")
+
+    if request.method == 'POST':
+        file = request.files['file']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename)))
+
     return render_template('resposta.html')
 
